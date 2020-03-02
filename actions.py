0cf52e05226701603bcb946f2d4e60ff3b37bcf5
Edward Healy <44093797+edwardhealy@users.noreply.github.com>
2020-03-02 12:47:13 +0000

    Needs SQL queries and testing

--- actions.py
+++ actions.py
@@ -1,27 +1,71 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/core/actions/#custom-actions/
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import cx_Oracle
 
 
-# This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+class viewTimetableDate(Action):
+ def name(self) -> Text:
+  return "action_view_timetable_date"
+ def run(self, dispatcher, tracker, domain):
+  userpwd = "oracle"
+  connection = cx_Oracle.connect("hr", userpwd, "CSORA12EDU.CS.CF.AC.UK", encoding="UTF-8")
+  with cx_Oracle.connect("hr", userpwd, "CSORA12EDU.CS.CF.AC.UK",
+            encoding="UTF-8") as connection:
+    cursor = connection.cursor()
+    cursor.execute("insert into SomeTable values (:1, :2)",
+            (1, "Some string"))
+    connection.commit()
+
+class viewTimetableTime(Action):
+ def name(self) -> Text:
+  return "action_view_timetable_time"
+ def run(self, dispatcher, tracker, domain):
+  userpwd = "oracle"
+  connection = cx_Oracle.connect("hr", userpwd, "CSORA12EDU.CS.CF.AC.UK", encoding="UTF-8")
+  with cx_Oracle.connect("hr", userpwd, "CSORA12EDU.CS.CF.AC.UK",
+            encoding="UTF-8") as connection:
+    cursor = connection.cursor()
+    cursor.execute("insert into SomeTable values (:1, :2)",
+            (1, "Some string"))
+    connection.commit()
+
+class suEventsDate(Action):
+ def name(self) -> Text:
+  return "action_view_su_events_date"
+ def run(self, dispatcher, tracker, domain):
+  userpwd = "oracle"
+  connection = cx_Oracle.connect("hr", userpwd, "CSORA12EDU.CS.CF.AC.UK", encoding="UTF-8")
+  with cx_Oracle.connect("hr", userpwd, "CSORA12EDU.CS.CF.AC.UK",
+            encoding="UTF-8") as connection:
+    cursor = connection.cursor()
+    cursor.execute("insert into SomeTable values (:1, :2)",
+            (1, "Some string"))
+    connection.commit()
+
+class suEventsTime(Action):
+ def name(self) -> Text:
+  return "action_view_su_events_time"
+ def run(self, dispatcher, tracker, domain):
+  userpwd = "oracle"
+  connection = cx_Oracle.connect("hr", userpwd, "CSORA12EDU.CS.CF.AC.UK", encoding="UTF-8")
+  with cx_Oracle.connect("hr", userpwd, "CSORA12EDU.CS.CF.AC.UK",
+            encoding="UTF-8") as connection:
+    cursor = connection.cursor()
+    cursor.execute("insert into SomeTable values (:1, :2)",
+            (1, "Some string"))
+    connection.commit()
+
+class locateLocation(Action):
+ def name(self) -> Text:
+  return "action_locate_location"
+ def run(self, dispatcher, tracker, domain):
+  userpwd = "oracle"
+  connection = cx_Oracle.connect("hr", userpwd, "CSORA12EDU.CS.CF.AC.UK", encoding="UTF-8")
+  with cx_Oracle.connect("hr", userpwd, "CSORA12EDU.CS.CF.AC.UK",
+            encoding="UTF-8") as connection:
+    cursor = connection.cursor()
+    cursor.execute("insert into SomeTable values (:1, :2)",
+            (1, "Some string"))
+    connection.commit()
